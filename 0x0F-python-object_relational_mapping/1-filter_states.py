5899c26036d345dce00bb7fddb9cec774f4fa96e
JAO <63289276+JAkinyi@users.noreply.github.com>
2022-10-06 14:22:43 +0300

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Lists all states with a name starting with N from the 
+"""Lists all states with a name starting with N from the
    database hbtn_0e_0_usa.
        Usage: ./1-filter_states.py <mysql username> \
                                    <mysql password> \
